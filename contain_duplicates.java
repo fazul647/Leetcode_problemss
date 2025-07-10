93e4c13b3e2ca182d64b23101a8bb51c82187536
fazul647 <fazulahammads@gmail.com>
2025-07-10 14:19:41 +0530

    Add files via upload

---
+++ contain_duplicates.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class contain_duplicates {
+    public boolean containsDuplicate(int[] nums) {
+        HashSet set=new HashSet<>();
+        for(int i=0;i<nums.length;i++){
+            if(set.contains(nums[i])){
+                return true;
+            }
+            else{
+                set.add(nums[i]);
+            }
+        }
+        return false;
+    }
+}
