c476d70d211fbe1198c4722d70a59ec2ce45a33b
SHAIK FAZUL AHAMMAD <fazulahammads@gmail.com>
2025-07-29 10:23:43 +0530

    Add solution for 73. Set Matrix Zeroes

---
+++ _73_Set_Matrix_Zeroes.java
@@ -0,0 +1,30 @@
+import java.util.HashSet;
+import java.util.Set;
+
+public class _73_Set_Matrix_Zeroes {
+       public void setZeroes(int[][] matrix) {
+        Set<Integer> row=new HashSet<>();
+        Set<Integer> col=new HashSet<>();
+        for(int i=0;i<matrix.length;i++){
+            for(int j=0;j<matrix[i].length;j++){
+                if(matrix[i][j]==0){
+                    row.add(i);
+                    col.add(j);
+                }
+                
+            }
+        }
+    for (int i = 0; i < matrix.length; i++) {
+    for (int j = 0; j < matrix[i].length; j++) {
+        if (row.contains(i) || col.contains(j)) {
+            matrix[i][j] = 0;
+        }
+    }
+}
+
+        
+        
+    }
+
+    
+}
