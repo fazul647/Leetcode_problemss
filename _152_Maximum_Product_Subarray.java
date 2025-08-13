45e7a1e3233a9b7dee6c315a4949341f968806c8
SHAIK FAZUL AHAMMAD <fazulahammads@gmail.com>
2025-08-13 18:38:30 +0530

    Update: Improved solution for Leetcode 152 - Maximum Product Subarray

--- _152_Maximum_Product_Subarray.java
+++ _152_Maximum_Product_Subarray.java
@@ -1,28 +1,17 @@
 public class _152_Maximum_Product_Subarray{
 public int maxProduct(int[] nums) {
-    int maxprod = nums[0];
-   int minprod = nums[0];
-   int result = nums[0];
-
-   for (int i = 1; i < nums.length; i++) {
-       int num = nums[i];
-
-       // Step 1: If num is negative, swap max and min
-       if (num < 0) {
-           int temp = maxprod;
-           maxprod = minprod;
-           minprod = temp;
-       }
-
-       // Step 2: Update max and min products for current index
-       maxprod = Math.max(num, num * maxprod);
-       minprod = Math.min(num, num * minprod);
-
-       // Step 3: Update result
-       result = Math.max(result, maxprod);
-   }
-
-   return result;
+    int prefix=1;
+    int suffix=1;
+    int result=Integer.MIN_VALUE;
+    int n=nums.length;
+    for(int i=0;i<nums.length;i++){
+        if(prefix==0)prefix=1;
+        if(suffix==0)suffix=1;
+        prefix=prefix*nums[i];
+        suffix=suffix*nums[n-i-1];
+        result=Math.max(result,Math.max(prefix,suffix));
+    }
+    return result;
    
 }
 }
