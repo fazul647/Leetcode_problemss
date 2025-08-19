6acbbd50d97d458220340d697c558fd4876d1c84
SHAIK FAZUL AHAMMAD <fazulahammads@gmail.com>
2025-08-19 09:49:19 +0530

    _704_Binary_Search solution added

---
+++ _704_Binary_Search.java
@@ -0,0 +1,19 @@
+class _704_Binary_Search {
+    public int search(int[] nums, int target) {
+        int l=0;
+        int r=nums.length-1;
+        while(r>=l){
+            int mid=(r+l)/2;
+            if(target==nums[mid]){
+                return mid;
+            }
+            else if(nums[mid]<target){
+                l=mid+1;
+            }
+            else{
+                r=mid-1;
+            }
+        }
+        return -1;
+    }
+}
